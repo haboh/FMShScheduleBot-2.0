686a410b8340a804de46266e3c586939070c49c0
haboh <43231178+haboh@users.noreply.github.com>
2018-09-13 21:30:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-BotToken = '635946335:AAG61rfYTsga7rLhFWwhjhEEHEcuqnvN0oQ'
+BotToken = ''
 ChanelId = '@FmshSchedule'
 FileWithListOfHashesOfImages = 'ListOfImages.json'
 DirectoryWithImages = 'C:\\Users\\mainc\\PycharmProjects\\ScheduleBot\\img'
